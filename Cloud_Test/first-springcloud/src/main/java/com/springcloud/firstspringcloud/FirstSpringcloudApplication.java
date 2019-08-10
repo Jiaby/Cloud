b8c347b07b9988681a10d75f78fabc727a42668d
ambition@x8ti.com <ambition@x8ti.com>
2019-08-10 14:56:45 +0800

    A New Cloud Project

---
+++ Cloud_Test/first-springcloud/src/main/java/com/springcloud/firstspringcloud/FirstSpringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.firstspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringcloudApplication.class, args);
+    }
+
+}
